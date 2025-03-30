30b72738e1444dd5f19175d466959abeafefde1b
Gage Fulwood <gagetfulwood@gmail.com>
2025-03-30 02:06:54 -0500

    Navbar CSS basic implementation

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('main.urls')),
+    path('', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+# in production, run 'python manage.py collectstatic'
